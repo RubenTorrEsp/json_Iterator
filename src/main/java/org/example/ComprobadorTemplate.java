18b85f43d12a71d9ede8510da04d1a35c6675264
Ruben Torrejon Espinosa <ruben.mte@gmail.com>
2024-08-30 15:04:07 +0200

    Creacion y completado de clase que comprueba que los valores de un json responde a un esquema predeterminado.

--- src/main/java/org/example/ComprobadorTemplate.java
+++ src/main/java/org/example/ComprobadorTemplate.java
@@ -8,7 +8,6 @@ import java.util.Map;
 public class ComprobadorTemplate {
 
     public ComprobadorTemplate(String rutaJsonReal, String rutaJsonSchema) {
-
         try {
             FileReader readerReal = new FileReader(rutaJsonReal);
             FileReader readerEsquema = new FileReader(rutaJsonSchema);
@@ -17,20 +16,16 @@ public class ComprobadorTemplate {
             JsonElement jsonEsquema = JsonParser.parseReader(readerEsquema);
 
             validarJson(jsonReal, jsonEsquema);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    public static void validarJson(JsonElement jsonReal, JsonElement jsonEsquema) {
-        if (jsonReal.isJsonObject() && jsonEsquema.isJsonObject()) {
-            validarJsonObject(jsonReal.getAsJsonObject(), jsonEsquema.getAsJsonObject());
-        } else if (jsonReal.isJsonArray() && jsonEsquema.isJsonArray()) {
-            validarJsonArray(jsonReal.getAsJsonArray(), jsonEsquema.getAsJsonArray());
-        } else {
-            System.out.println("Error: Los elementos JSON no son del mismo tipo o no son objetos ni arrays.");
+    public static void validarJson(JsonElement jsonReal, JsonElement jsonSchema) {
+        if (jsonReal.isJsonObject() && jsonSchema.isJsonObject()) {
+            validarJsonObject(jsonReal.getAsJsonObject(), jsonSchema.getAsJsonObject());
+        } else if (jsonReal.isJsonArray() && jsonSchema.isJsonArray()) {
+            validateJsonArray(jsonReal.getAsJsonArray(), jsonSchema.getAsJsonArray());
         }
     }
 
@@ -44,15 +39,13 @@ public class ComprobadorTemplate {
 
                 if (valorEsquema.isJsonPrimitive()) {
                     String tipoEsperado = valorEsquema.getAsString();
-                    if (!comprobarTipo(valorReal, tipoEsperado)) {
-                        System.out.println("Error: El campo '" + clave + "' no coincide con el tipo esperado. Tipo esperado: " + tipoEsperado + ", Tipo encontrado: " + obtenerTipo(valorReal));
+                    if (!checkType(valorReal, tipoEsperado)) {
+                        System.out.println("Error: El campo '" + clave + "' no coincide con el tipo esperado.");
                     }
                 } else if (valorEsquema.isJsonObject() && valorReal.isJsonObject()) {
-                    // Validar recursivamente el objeto JSON anidado
                     validarJsonObject(valorReal.getAsJsonObject(), valorEsquema.getAsJsonObject());
                 } else if (valorEsquema.isJsonArray() && valorReal.isJsonArray()) {
-                    // Validar recursivamente el array JSON anidado
-                    validarJsonArray(valorReal.getAsJsonArray(), valorEsquema.getAsJsonArray());
+                    validateJsonArray(valorReal.getAsJsonArray(), valorEsquema.getAsJsonArray());
                 } else {
                     System.out.println("Error: Estructura inesperada o no coincidente para el campo '" + clave + "'.");
                 }
@@ -62,21 +55,19 @@ public class ComprobadorTemplate {
         }
     }
 
-    private static void validarJsonArray(JsonArray arrayReal, JsonArray arrayEsquema) {
-        if (!arrayEsquema.isEmpty()) {
-            JsonElement tipoEsquema = arrayEsquema.get(0); // Suponemos que todos los elementos del array son del mismo tipo
-            for (JsonElement elementoReal : arrayReal) {
-                if (tipoEsquema.isJsonPrimitive()) {
-                    String tipoEsperado = tipoEsquema.getAsString();
-                    if (!comprobarTipo(elementoReal, tipoEsperado)) {
-                        System.out.println("Error en array: El tipo de elemento no coincide con el tipo esperado. Tipo esperado: " + tipoEsperado + ", Tipo encontrado: " + obtenerTipo(elementoReal));
+    private static void validateJsonArray(JsonArray arrayReal, JsonArray arrayScchema) {
+        if (!arrayScchema.isEmpty()) {
+            JsonElement schemaType = arrayScchema.get(0);
+            for (JsonElement element : arrayReal) {
+                if (schemaType.isJsonPrimitive()) {
+                    String typeExpected = schemaType.getAsString();
+                    if (!checkType(element, typeExpected)) {
+                        System.out.println("Error en array: El tipo de elemento no coincide con el tipo esperado.");
                     }
-                } else if (tipoEsquema.isJsonObject() && elementoReal.isJsonObject()) {
-                    // Validar recursivamente el objeto JSON dentro del array
-                    validarJsonObject(elementoReal.getAsJsonObject(), tipoEsquema.getAsJsonObject());
-                } else if (tipoEsquema.isJsonArray() && elementoReal.isJsonArray()) {
-                    // Validar recursivamente el array JSON anidado dentro del array
-                    validarJsonArray(elementoReal.getAsJsonArray(), tipoEsquema.getAsJsonArray());
+                } else if (schemaType.isJsonObject() && element.isJsonObject()) {
+                    validarJsonObject(element.getAsJsonObject(), schemaType.getAsJsonObject());
+                } else if (schemaType.isJsonArray() && element.isJsonArray()) {
+                    validateJsonArray(element.getAsJsonArray(), schemaType.getAsJsonArray());
                 } else {
                     System.out.println("Error en array: Estructura inesperada o no coincidente en el array.");
                 }
@@ -84,7 +75,7 @@ public class ComprobadorTemplate {
         }
     }
 
-    private static boolean comprobarTipo(JsonElement valor, String tipoEsperado) {
+    private static boolean checkType(JsonElement valor, String tipoEsperado) {
         return switch (tipoEsperado.toLowerCase()) {
             case "string" -> valor.isJsonPrimitive() && valor.getAsJsonPrimitive().isString();
             case "boolean" -> valor.isJsonPrimitive() && valor.getAsJsonPrimitive().isBoolean();
@@ -96,21 +87,4 @@ public class ComprobadorTemplate {
         };
     }
 
-    private static String obtenerTipo(JsonElement valor) {
-        if (valor.isJsonPrimitive()) {
-            JsonPrimitive primitivo = valor.getAsJsonPrimitive();
-            if (primitivo.isString()) {
-                return "string";
-            } else if (primitivo.isBoolean()) {
-                return "boolean";
-            } else if (primitivo.isNumber()) {
-                return "number";
-            }
-        } else if (valor.isJsonObject()) {
-            return "object";
-        } else if (valor.isJsonArray()) {
-            return "array";
-        }
-        return "unknown";
-    }
 }
